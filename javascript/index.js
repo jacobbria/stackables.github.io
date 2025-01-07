dfeb74c9700708a22cc8e869d15d5e986da050ec
jacobbria <jacob.bria@sinclair.edu>
2025-01-07 16:30:12 -0500

    Updated current layout

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,5 @@
+// Track 
+
 // Control menu visibility while scrolling
 let lastScrollTop = 0;
 const navbar = document.getElementById("gorrdyNavbar");
@@ -22,13 +24,54 @@ window.addEventListener("scroll", function () {
 const contactBtn = document.getElementById('contactBtn');
 const contactForm = document.getElementById('contactForm');
 const closeForm = document.getElementById('closeForm');
+const contactFormForm = document.querySelector('#contactForm form');
 
 contactBtn.addEventListener('click', () => {
     console.log("contactBtn clicked!");
     contactForm.style.display = 'block';
   });
-
 closeForm.addEventListener('click', () => {
     console.log("closeForm clicked!");
     contactForm.style.display = 'none';
+  });
+contactFormForm.addEventListener('submit', (event) => {
+    console.log("Contact form submit attempt");
+
+    if (!contactFormForm.reportValidity()){
+        event.preventDefault();
+        console.log("Contact form INVALID");
+    } else {
+      console.log("Contact form validation passed!");
+      event.preventDefault(); // no backend is handling the form
+      contactForm.style.display = 'none';
+    }
+})
+
+  // Report Issue Visibility
+const reportIssuetBtn = document.getElementById('reportIssueBtn');
+const reportIssueForm = document.getElementById('reportIssueForm');
+const closeIssueForm = document.getElementById('closeReportForm');
+const issueForm = document.querySelector('#reportIssueForm form');
+
+reportIssuetBtn.addEventListener('click', () => {
+    console.log("reportIssueBtn clicked!");
+    reportIssueForm.style.display = 'block';
+  });
+
+  closeIssueForm.addEventListener('click', () => {
+    console.log("closeIssueForm clicked!");
+    reportIssueForm.style.display = 'none';
+  });
+  issueForm.addEventListener('submit', (event) => {
+    console.log("sendIssueForm clicked!");
+  
+    // browser validation
+    if (!issueForm.reportValidity()) {
+      event.preventDefault();
+      console.log("Form validation failed!");
+    } else {
+      console.log("Form validation passed!");
+      event.preventDefault(); // no backend is handling the form
+      reportIssueForm.style.display = 'none';
+    }
   });
